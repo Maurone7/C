c9abd4533f163539d7f2b478eb9380944d45a2e0
Mauro Patimo <mauro.patimo@gmail.com>
2022-10-06 23:26:45 -0700

    Updates

---
+++ import-data.c
@@ -0,0 +1,42 @@
+//
+// Created by Mauro Patimo on 10/6/22.
+//
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_LINES 100
+#define MAX_LEN 256
+
+int import_data()
+{
+    char data[MAX_LINES][MAX_LEN];
+    char *filename = "/Users/mauro/Dropbox/GitHub/C/Barenfeld Table 1.txt";
+    FILE * file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        printf("Not Opened \n");
+        return 1;
+    }
+
+    int ch = 0;
+
+    while ((ch = fgetc(file)) != EOF)
+        putchar(ch);
+
+    char buffer[MAX_LEN];
+    char * column1;
+
+    int data_1;
+    char column_1[MAX_LEN];
+    data_1 = fgetc(file);
+    sprintf(column_1, "%d", data_1);
+
+    printf("%d", data_1);
+
+
+    fclose(file);
+
+
+    return 0;
+}
