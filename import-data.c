d9c91dcb8e2faf8c1a80734d5b3e28e8b2e6e701
Mauro Patimo <mauro.patimo@gmail.com>
2022-10-10 10:35:43 -0700

    Updates

--- import-data.c
+++ import-data.c
@@ -6,9 +6,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-int import_data()
+int main()
 {
-    char source_list_barenfeld_2016[106] = {};
+    char source_list_barenfeld_2016[106] = {'\0'};
 
     char source[100];
     char *filename = "/Users/mauro/Dropbox/GitHub/C/Barenfeld Table 1.txt";
@@ -23,19 +23,19 @@ int import_data()
     char *element;
     int line = 0;
     int index = -1;
-    while (line < 130)
+    for (line = 0; line < 13; line++)
     {
         fgets(source, 256, file);
         if (line >21 && line < 129)
         {
             element = strtok(source, " ");
             element = strtok(NULL, " ");
+            printf("%s", element);
 
         }
         index++;
         source_list_barenfeld_2016[index] = *element;
         printf("%d %c \n", index, source_list_barenfeld_2016[22]);
-        line ++;
         /* printf("%d \n", line); */
     }
     fclose(file);
@@ -44,8 +44,6 @@ int import_data()
     {
         printf("%d %c \n", loop_value, source_list_barenfeld_2016[loop_value]);
     }
-    printf("%lu \n", (sizeof source_list_barenfeld_2016 )/(sizeof source_list_barenfeld_2016[0]));
-
 
     return 0;
 }
